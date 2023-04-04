56337aed65fa4f965fff65b6333f0c266f8b156a
denn-acrymoore <denn.sebastian.e@gmail.com>
2023-04-04 19:27:54 +0700

    Buat FPREP_ADV, update PELENGKAP dan unittest
    Changes (CFG File):
    - Buang KLAUSA_TDK_LENGKAP dan KLAUSA_SUPLE_TDK_LENGKAP dari
      PELENGKAP dan PELENGKAP_SUPLE karena menurunkan performa
      CFG secara drastis dan kurang tepat digunakan pada PELENGKAP
      (waktu pengerjaan bertambah 822%).
    
    - Buat FPREP_ADV untuk memisahkan preposisi yang menggunakan
      adverbia agar hanya bisa digunakan di PREDIKAT dan PELENGKAP
      (FN tidak boleh menggunakan FPREP_ADV).
    
    - Tambah FPREP_ADV, FPREP_ADV_KJ_KOOR_TAK_HINGGA,
      FPREP_ADV_KJ_KOOR_TERHINGGA pada PREDIKAT_FPREP dan
      PELENGKAP_FPREP.
    
    - Perbaiki dokumentasi KJ_KOOR_TERHINGGA_KLAUSA dan
      KAL_MAJEMUK_TERHINGGA_KLAUSA (buang 'selanjutnya', 'setelahnya',
      'sebelumnya'; tambah 'apalagi').
    
    - Tambah dokumentasi tentang FPREP_ADV, FPREP_DGN_TNP_ADV,
      FPREP_ADV_KJ_KOOR_TAK_HINGGA, FPREP_ADV_KJ_KOOR_TERHINGGA,
      REKURSI_FPREP_ADV_KJ_KOOR_TAK_HINGGA,
      REKURSI_FPREP_ADV_KJ_KOOR_TAK_HINGGA_2.
    
    - Tambah comment alasan PELENGKAP_SUPLE tidak mencakup FPREP karena
      FPREP perlu perlakuan khusus (PELENGKAP_FPREP).
    
    - Non-aktifkan FPREP_KJ_KOOR_TAK_HINGGA dan FPREP_KJ_KOOR_TERHINGGA
      pada PREDIKAT dan PELENGKAP karena redundant dengan
      FPREP_ADV_KJ_KOOR_TAK_HINGGA dan FPREP_ADV_KJ_KOOR_TERHINGGA.
    
    Changes (Unit Test):
    - Tambah folder baru "test" untuk pemecahan unittest menjadi
      modul-modul (file-file terpisah).
    
    - Tambah chart parser strategy lain pada unittest.
    
    - Tambah unittest "pelengkap klausa".

--- test/cfg_unittest.py
+++ test/cfg_unittest.py
@@ -1,7 +1,14 @@
 import nltk
 from nltk.parse.chart import SteppingChartParser
-from nltk.parse.chart import FilteredBottomUpPredictCombineRule
-from nltk.parse.chart import LeafInitRule, FilteredSingleEdgeFundamentalRule
+from nltk.parse.chart import (
+    FilteredBottomUpPredictCombineRule,
+    LeafInitRule,
+    FilteredSingleEdgeFundamentalRule,
+    EmptyPredictRule,
+    BottomUpPredictCombineRule,
+    SingleEdgeFundamentalRule,
+    BottomUpPredictRule,
+)
 import unittest
 import os
 
@@ -11,6 +18,28 @@ grammar = nltk.data.load(
     "file:" + os.path.join(model_path, "utapis_sintaksis_kalimat_v2_skripsi.cfg"), "cfg"
 )
 
+# === Bottom-Up Chart Parser ===
+# Test Finish Time (Pelengkap hanya bisa klausa lengkap): 60.643s
+BU_STRATEGY = [
+    LeafInitRule(),
+    EmptyPredictRule(),
+    BottomUpPredictRule(),
+    SingleEdgeFundamentalRule(),
+]
+
+# === Bottom-Up Left-Corner Chart Parser ===
+# Test Finish Time (Pelengkap bisa klausa tdk lengkap): 93.022s
+# Test Finish Time (Pelengkap hanya bisa klausa lengkap): 36.355s
+BU_LC_STRATEGY = [
+    LeafInitRule(),
+    EmptyPredictRule(),
+    BottomUpPredictCombineRule(),
+    SingleEdgeFundamentalRule(),
+]
+
+# === Left-Corner Chart Parser ===
+# Test Finish Time (Pelengkap bisa klausa tdk lengkap): 74.112s
+# Test Finish Time (Pelengkap hanya bisa klausa lengkap): 14.695s
 LEFT_CORNER_STRATEGY = [
     LeafInitRule(),
     FilteredBottomUpPredictCombineRule(),
@@ -34,6 +63,12 @@ def stepping_chart_parsing(scp, tags):
         # yang bisa ditambahkan.
         if step is None:
             break
+
+    # Print the result.
+    for x in scp.parses():
+        print(x)
+        break
+    print()
 
     # Return generator.
     return scp.parses()
@@ -771,6 +806,27 @@ class TestCFG(unittest.TestCase):
             "<verba>",
             "<nomina>",
             "<nomina>",
+            "<nomina>",
+            "<td_akhir_kal>",
+        ]
+        self.assertTrue(cfg_true_or_false(utapis_scp, kal))
+
+    def test_pelengkap_klausa(self):
+        """
+        (Dia, nomina), (sering, adverbia), (bertanya, verba), (kapan, nomina),
+        (kita, nomina), (akan, adverbia), (bertemu, verba), (lagi, adverbia),
+        (di, preposisi), (taman, nomina), (., td_akhir_kal).
+        """
+        kal = [
+            "<nomina>",
+            "<adverbia>",
+            "<verba>",
+            "<nomina>",
+            "<nomina>",
+            "<adverbia>",
+            "<verba>",
+            "<adverbia>",
+            "<preposisi>",
             "<nomina>",
             "<td_akhir_kal>",
         ]
