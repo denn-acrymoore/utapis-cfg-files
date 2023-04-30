b07aeca5802be17a1dbaa5c495f70a37369d7477
denn-acrymoore <denn.sebastian.e@gmail.com>
2023-04-30 11:06:59 +0700

    Update Kode Unittest (Path Code)
    Changes:
    - Ubah "os.path.join(__file__, os.pardir)" menjadi
      "os.path.dirname(__file__)".

--- test/cfg_unittest.py
+++ test/cfg_unittest.py
@@ -2,7 +2,7 @@ import unittest
 import os
 
 if __name__ == "__main__":
-    test_path = os.path.join(__file__, os.pardir)
+    test_path = os.path.dirname(__file__)
     suite = unittest.TestLoader().discover(test_path, "test_*.py")
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite)
